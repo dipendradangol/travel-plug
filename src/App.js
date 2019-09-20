1cc21cb02b044ec95b3083aaaffacb4e363e738f
Akeundo <48980681+Akeundo@users.noreply.github.com>
2019-09-20 22:57:37 +0300

    api call
    App.js -> api call
    App.css ->text alignment to left

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          items: [],
+          isLoaded: false,
+  }
 }
 
+  componentDidMount() {
+   const url= 'https://api.openchargemap.io/v3/poi/?output=json&countrycode=FI&maxresults=500&compact=true&verbose=false'
+      fetch(url)
+          .then(res => res.json())
+          .then(json => {
+            this.setState({
+              isLoaded: true,
+              items: json,
+            })
+          });
+  }
+
+  render() {
+    var {isLoaded, items} = this.state;
+    if (!isLoaded) {
+      return <div>loading....</div>
+    } else {
+      return (
+        <div className="App">
+          <ul>
+            {items.map(item => (
+              <li key={item.ID}>
+                Customer ID : {item.ID} <br/>
+                Customer AddressLine1  : {item.AddressInfo.AddressLine1} <br/>
+                Customer AddressLine2  : {item.AddressInfo.AddressLine1} <br/>
+                Customer Postcode : {item.AddressInfo.Postcode} <br/>
+                Customer City  : {item.AddressInfo.Town} <br/>
+                Location Latitude : {item.AddressInfo.Latitude} <br/>
+                Location Longitude : {item.AddressInfo.Longitude} <br/>
+                
+                <br/>
+               
+               </li>
+            ))};
+          </ul>
+        </div>
+        );
+    }
+     
+}
+}
 export default App;
+/*
+Charging Port : {item.Connections[0].Quantity ? item.Connections[0].Quantity : 'Not found'} <br/>
+ Plug Model: {item.Connections[0].ConnectionType ? item.Connections[0].ConnectionType.Title : 'not found'} <br/>
+ Plug Status: {item.Connections[0].StatusType ? item.Connections[0].StatusType.Title : 'not found'} <br/>
+*/
