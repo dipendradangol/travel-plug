9c56afc5597b25eee25e82ec97738947f212b92e
dipendradangol <dipendra.dangol11@gmail.com>
2019-10-02 14:12:12 +0300

    Updates

---
+++ precache-manifest.c7a307fc3d94e141647fc88bf3801088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec05a69c45b3c1f541223eaabe9dc64",
+    "url": "/travel-plug/index.html"
+  },
+  {
+    "revision": "052cdf10eb0d41788ee5",
+    "url": "/travel-plug/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1c0fe87c0df5a2096870",
+    "url": "/travel-plug/static/css/main.fb035306.chunk.css"
+  },
+  {
+    "revision": "052cdf10eb0d41788ee5",
+    "url": "/travel-plug/static/js/2.ce4345e0.chunk.js"
+  },
+  {
+    "revision": "1c0fe87c0df5a2096870",
+    "url": "/travel-plug/static/js/main.2e07fd02.chunk.js"
+  },
+  {
+    "revision": "03820a59eea0cbe01d3c",
+    "url": "/travel-plug/static/js/runtime~main.978cf7c5.js"
+  }
+]);
