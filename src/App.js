4ad1f1cc5e60119936af4845c235913689656c54
dipendradangol <dipendra.dangol11@gmail.com>
2019-10-02 12:36:07 +0300

    removed unwanted files

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
-import React, { useState } from 'react';
-import Distance from './Components/Distance';
-import Api from './Components/Api';
-import ReactMapGL, {Marker} from 'react-map-gl';
-import * as PlugShare from "./Components/plug-location.json";
-
-
+import React from 'react';
 
 function App() {
-   const [viewport, setviewport] = useState({
-     latitude:60.242263300000005,
-     longitude:25.0716969,
-     width: '100vw',
-     height: '80vh',
-     zoom: 10,
-   });
-
-    
-     
         return (
           <div className="App">
-            
-            <ReactMapGL  {...viewport} 
-            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-            mapStyle = "mapbox://styles/akeundo/ck0vicxkj0pd41clpjk9abqki"
-            onViewportChange={viewport => {
-              setviewport(viewport);
-            }}
-             >
-            {PlugShare.features.map(plug => (
-              <marker 
-              key={plug.ID }
-              latitude={plug.AddressInfo.Latitude}
-              longitude={plug.AddressInfo.Longitude}
-              >
-               <div class="marker">plugs</div>
-              </marker>
-            ))}
-            </ReactMapGL>
-            <Distance/>
-            <Api/>
-            
+            Hello world
           </div>
           );
       }
@@ -50,9 +14,3 @@ function App() {
  
 export default App;
 
-
-/*
-<button class="marker">
- <img src="./electric-car.jpg" alt ="plug icon" />
-</button> 
-*/
